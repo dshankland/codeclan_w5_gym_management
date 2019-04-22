69b57f7a6e4ab3ca6aa012bc69222debd266173e
Darren Shankland <ds@4pq.co.uk>
2019-04-22 20:32:02 +0100

    add sql_runner changes

--- db/sql_runner.rb
+++ db/sql_runner.rb
@@ -4,8 +4,8 @@ class SqlRunner
 
   def self.run(sql, values = [])
     begin
-      db = PG.connect({dbname: 'gym4all', host: 'localhost'})
-      # db = PG.connect({dbname: 'd4pjooq5hhdpu0', host: 'ec2-50-17-231-192.compute-1.amazonaws.com', port: 5432, user: 'ibmktlmxbowdxg', password: 'ddd467ab38a38877cedca7e8b4f6299442d0b4d66f92953247f2dd2600a018c8'})
+      # db = PG.connect({dbname: 'gym4all', host: 'localhost'})
+      db = PG.connect({dbname: 'd2pq0rcjqkolca', host: 'ec2-54-225-129-101.compute-1.amazonaws.com', port: 5432, user: 'ahcefxzxiqkfen', password: '321a9ee536d4bc613179217b53861608e3c8cb89a9ed58df20612ee15685ab70'})
       db.prepare('query', sql)
       result = db.exec_prepared('query', values)
     ensure
